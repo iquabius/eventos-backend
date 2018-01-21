1724482ef0fedbc73bbee62bb65bf462f392cfde
Josias Iquabius <iquabius@gmail.com>
2018-01-21 19:20:07 -0300

    Setup knex for dev and test

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+const migrations = {
+  directory: path.resolve(__dirname, 'database', 'migrations')
+};
+
+const seeds = {
+  directory: path.resolve(__dirname, 'database', 'seeds')
+};
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/dev.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations,
+    seeds
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.sqlite3',
+    },
+    useNullAsDefault: true,
+    migrations,
+    seeds
+  }
+};
